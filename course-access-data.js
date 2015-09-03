6706e9d9b4474010ecee96eb47e0f9e34ce46ce7
Sandbox Sync <support@getsandbox.com>
2015-09-03 16:52:46 -0400

    Changes

--- course-access-data.js
+++ course-access-data.js
@@ -2,17 +2,19 @@ var consts = {
     MS_PER_DAY: 86400000
 };
 
-var generate = function generate(orgUnit) {
+var generate = function generate(orgUnit, start, end) {
     var results = {},
         numResults = 365,
-        date = new Date().setUTCHours(0,0,0,0);
+        startDate = new Date(start).setUTCHours(0,0,0,0),
+        endDate = new Date(end).setUTCHours(0,0,0,0),
+        dateDiff = endDate - startDate;
 
     results[orgUnit] = {
         "Course Offering": {}
     };
 
-    for(var i = 0; i < numResults; i++){
-        results[orgUnit]["Course Offering"][date - consts.MS_PER_DAY * i] = Math.round(Math.random() * 1000);
+    for(var i = 0; i < numResults && i * consts.MS_PER_DAY <= dateDiff; i++){
+        results[orgUnit]["Course Offering"][endDate - consts.MS_PER_DAY * i] = Math.round(Math.random() * 1000);
     }
     
     return results;

54ff18b4a7960ae714a1e6cd2f2dabd9a71f6a9c
Chris Silcock <chris.silcock@d2l.com>
2015-09-03 16:26:57 -0400

    no message

--- course-access-data.js
+++ course-access-data.js
@@ -1,7 +1,22 @@
-var consts = {};
+var consts = {
+    MS_PER_DAY: 86400000
+};
 
-var generate = function generate(granularity) {
+var generate = function generate(offering) {
+    var numResults = 365,
+        date = new Date();
+        
+    var results = {
+        offering: {
+            "Course Offering": {}
+        }
+    };
+        
+    for(var i = 0; i < numResults; i++){
+        results[offering]["Course Offering"][date.getDate() - consts.MS_PER_DAY * i] = Math.round(Math.random() * 1000);
+    }
     
+    return results;
 };
 
 module.exports = {

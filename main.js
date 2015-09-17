c07089a9d7d764044e014dd8593579e44e055dd3
Sandbox Sync <support@getsandbox.com>
2015-09-17 16:58:02 -0400

    Changes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var generateCourseAccessData = require('course-access-data.js').generate,
  *  /d2l/api/adp/unstable/aggregatedEvents
  */
 
-Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{orgId}/Course Offering','GET', function(req, res) {
+Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{orgId}/Course Offering/roleId','GET', function(req, res) {
     var startTime = req.query.startTime === undefined ? 
             Math.max(0, new Date().getTime() - toolAccessConsts.MS_PER_DAY * toolAccessConsts.MAX_RESULTS) : 
             parseInt(req.query.startTime),
@@ -14,12 +14,9 @@ Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{orgId}/Course Offer
             new Date().getTime() : 
             parseInt(req.query.endTime);
     
-    
-    //var result = generateCourseAccessData(req.params.orgId, startTime, endTime);
-    
-    var result = [startTime, endTime];
-    
+    var result = generateCourseAccessData(req.params.orgId, req.params.roleId, startTime, endTime);
+
     res.type('application/json');
     res.status(200);
     res.json(result);
-})
+});

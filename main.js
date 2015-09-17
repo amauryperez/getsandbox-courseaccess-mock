f84d5c331ecc766023bcb212318b8a6ae3502b24
Sandbox Sync <support@getsandbox.com>
2015-09-17 17:04:27 -0400

    Changes

--- main.js
+++ main.js
@@ -6,17 +6,36 @@ var generateCourseAccessData = require('course-access-data.js').generate,
  */
 
 Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{orgId}/Course Offering','GET', function(req, res) {
-    var startTime = req.query.startTime === undefined ? 
-            Math.max(0, new Date().getTime() - toolAccessConsts.MS_PER_DAY * toolAccessConsts.MAX_RESULTS) : 
-            parseInt(req.query.startTime),
-            
-        endTime = req.query.endTime === undefined ? 
-            new Date().getTime() : 
-            parseInt(req.query.endTime);
+    var dates = parseDates(req.query.startTime, req.query.endTime);
     
-    var result = generateCourseAccessData(req.params.orgId, undefined, startTime, endTime);
+    var result = generateCourseAccessData(req.params.orgId, undefined, dates.startTime, dates.endTime);
 
     res.type('application/json');
     res.status(200);
     res.json(result);
 });
+
+Sandbox.define('/d2l/api/adp/unstable/aggregatedEvents/{id}/{orgId}/Course Offering/{roleId}','GET', function(req, res) {
+    var dates = parseDates(req.query.startTime, req.query.endTime);
+    
+    var result = generateCourseAccessData(req.params.orgId, roleId, dates.startTime, dates.endTime);
+
+    res.type('application/json');
+    res.status(200);
+    res.json(result);
+});
+
+var parseDates = function(startDate, endDate){
+    var startTime = startDate === undefined ? 
+            Math.max(0, new Date().getTime() - toolAccessConsts.MS_PER_DAY * toolAccessConsts.MAX_RESULTS) : 
+            parseInt(startDate),
+            
+        endTime = endDate === undefined ? 
+            new Date().getTime() : 
+            parseInt(endDate);
+            
+    return {
+        "startTime":startTime, 
+        "endTime":endTime
+    };
+};

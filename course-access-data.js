1b680818b22525900d7f756483a1bb4e1394a3fa
Sandbox Sync <support@getsandbox.com>
2015-09-17 16:42:00 -0400

    Changes

--- course-access-data.js
+++ course-access-data.js
@@ -15,7 +15,7 @@ var generate = function generate(orgUnit, roleId, start, end) {
     }
     else {
         for(var i = 0; i < 25; i++) {
-            roleIds.push(i + 578);
+            roleIds.push((i + 578).toString());
         }
     }
 
@@ -27,7 +27,7 @@ var generate = function generate(orgUnit, roleId, start, end) {
 
     for(var j = 0; j < roleIds.length; j++){
         for(var k = numVals; k >= 0; k--){
-            results[orgUnit]["Course Offering"][endDate - consts.MS_PER_DAY * k] = (Math.round(Math.random() * 1000)).toString();
+            results[orgUnit]["Course Offering"][roleIds[j]][endDate - consts.MS_PER_DAY * k] = (Math.round(Math.random() * 1000)).toString();
         }
     }
     
